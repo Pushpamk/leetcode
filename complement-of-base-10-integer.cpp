4f8b28e7a8b01c3434e2eb7a79f2a6a0c47ce332
Pushpam Kumar <kumarpushpam99@gmail.com>
2022-01-04 10:58:19 +0530

    added comment

--- complement-of-base-10-integer.cpp
+++ complement-of-base-10-integer.cpp
@@ -4,7 +4,7 @@ public:
         if (n == 0)
             return 1;
         int length = log2(n) + 1;
-        int temp = pow(2, length) - 1;
+        int temp = pow(2, length) - 1; // can be written as (1 << length) - 1
         return n ^ temp;
     }
 };
