456f6b931524eeb4ce5d0475354cd00b8296af77
Pushpam Kumar <kumarpushpam99@gmail.com>
2022-01-04 10:45:20 +0530

    added complement of base 10 integer prob

---
+++ complement-of-base-10-integer.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0)
+            return 1;
+        int length = log2(n) + 1;
+        int temp = pow(2, length) - 1;
+        return n ^ temp;
+    }
+};
