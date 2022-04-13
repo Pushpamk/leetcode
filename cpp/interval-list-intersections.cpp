73360876b06e5bf0b41ec264d466252a80bd996e
Pushpam Kumar <kumarpushpam99@gmail.com>
2022-04-14 00:12:57 +0530

    added interval list intersections prob

--- cpp/interval-list-intersections.cpp
+++ cpp/interval-list-intersections.cpp
@@ -7,10 +7,7 @@ public:
             int a_start = firstList[i][0], a_end = firstList[i][1];
             int b_start = secondList[j][0], b_end = secondList[j][1];
             if ( a_start <= b_end && a_end >= b_start) {
-                vector<int> tmp(2);
-                tmp[0] = max(a_start, b_start);
-                tmp[1] = min(a_end, b_end);
-                ans.push_back(tmp);
+                ans.push_back({max(a_start, b_start), min(a_end, b_end)});
             }
             if (a_end <= b_end)
                 i++;
